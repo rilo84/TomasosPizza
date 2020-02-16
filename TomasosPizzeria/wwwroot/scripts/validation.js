cb78d75fd4401974a2a790f6c7dbbcf5d84bbe82
rilo84 <richard.losinno@gmail.com>
2020-02-16 15:39:09 +0100

    added client validation

---
+++ TomasosPizzeria/wwwroot/scripts/validation.js
@@ -0,0 +1,12 @@
+﻿var form = document.querySelectorAll('.needs-validation');
+
+form.forEach(item => {
+    item.addEventListener('submit', (e) => {
+
+        if (item.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        item.classList.add('was-validated')
+    })
+})
